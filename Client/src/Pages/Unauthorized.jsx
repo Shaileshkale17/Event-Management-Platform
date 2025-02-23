298013d04032fdeefe0fcf83f6ab29f66b407105
shailesh kale <shaileshkale87730@gmail.com>
2025-02-23 19:41:58 +0530

    adding the color

--- Client/src/Pages/Unauthorized.jsx
+++ Client/src/Pages/Unauthorized.jsx
@@ -6,15 +6,15 @@ import "animate.css";
 
 const Unauthorized = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100">
-      <div className="bg-white p-8 rounded-lg shadow-lg text-center animate__animated animate__fadeIn">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-black">
+      <div className="bg-black p-8 rounded-lg shadow-lg text-center animate__animated animate__fadeIn">
         {/* Animated Icon */}
         <FontAwesomeIcon
           icon={faExclamationTriangle}
           className="text-yellow-500 text-6xl mb-4 animate__animated animate__tada animate__delay-1s"
         />
-        <h1 className="text-3xl font-bold text-gray-800 mb-4">Access Denied</h1>
-        <p className="text-gray-600 mb-6">
+        <h1 className="text-3xl font-bold text-gray-300 mb-4">Access Denied</h1>
+        <p className="text-gray-400 mb-6">
           You do not have permission to view this page. Please check your access
           rights or contact the administrator.
         </p>
